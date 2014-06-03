6abe5fe42e03a2c61f4d72e966f22316650944b3
Tobin Brown <tobinjbrown@gmail.com>
2014-06-03 18:19:46 -0500

    Create gif.js
    Gif.js is a wobot plugin that will display a random gif given a search term. It is called with the command !gif 'keyword'. It will use the giphy api to retrieve 25 gifs under that search term and randomly choose 1. The chosen one is displayed in the chat.

---
+++ plugins/gif.js
@@ -0,0 +1,27 @@
+/**
+ * This plugin will display a random gif from the giphy
+ * api based on a search term.
+ */
+
+var request = require('request');
+
+module.exports.load = function(bot) {
+  bot.onMessage(/!gif .*/, gif);
+};
+
+var gif = function(channel, from, message) {
+  var self = this;
+  request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&offset=0&q=' + message.split(' ')[1], function (error, response, body) {
+    if (!error) {
+      var body = JSON.parse(body);
+      if (body.pagination.count == 0) {
+        self.message(channel, '@' + from.split(' ').join('') + ' 0 \'' + message.split(' ')[1] + '\' gifs were found :( try another search' );
+      } else {
+        var index = Math.floor(Math.random() * 24);
+        var gif = body.data[index].images.original.url;
+        self.message(channel, '@' + from.split(' ').join('') + ' Here\'s a random \'' + message.split(' ')[1] +  '\'  gif! ' + gif);
+      }
+    }
+  });
+  return true;
+};
