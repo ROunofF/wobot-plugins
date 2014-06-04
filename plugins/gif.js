2c6c77150fed2b1e80f9c8127d6caf8aa54c40ce
Tobin Brown <tobinjbrown@gmail.com>
2014-06-04 10:19:33 -0500

    Allow multiple arguments for gif.js search
    Gif.js now supports multiple search terms by splitting up the message and contactenating them on the api call url.

--- plugins/gif.js
+++ plugins/gif.js
@@ -11,16 +11,22 @@ module.exports.load = function(bot) {
 
 var gif = function(channel, from, message) {
   var self = this;
-  request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&offset=0&q=' + message.split(' ')[1], function (error, response, body) {
+  var arguments = message.split(' ');
+  var query = '';
+  for (var i = 1; i < arguments.length; i++) {
+    query += arguments[i] + ' ';
+  }
+  request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&offset=0&q=' + query, function (error, response, body) {
     if (!error) {
       var body = JSON.parse(body);
       var size = body.pagination.count;
+      
       if (size == 0) {
-        self.message(channel, '@' + from.split(' ').join('') + ' 0 \'' + message.split(' ')[1] + '\' gifs were found :( try another search' );
+        self.message(channel, '@' + from.split(' ').join('') + ' 0 \'' + query + '\' gifs were found :( try another search' );
       } else {
         var index = Math.floor(Math.random() * size);
         var gif = body.data[index].images.original.url;
-        self.message(channel, '@' + from.split(' ').join('') + ' Here\'s a random \'' + message.split(' ')[1] +  '\'  gif! ' + gif);
+        self.message(channel, '@' + from.split(' ').join('') + ' Here\'s a random \'' + query +  '\'  gif! ' + gif);
       }
     }
   });
