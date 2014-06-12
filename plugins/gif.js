eaf350d87f5a6ce90f7e64abe24d51cc160c03c5
Tobin Brown <tobin@bulubox.com>
2014-06-12 11:14:46 -0500

    Added better gif search functionality
    You can now put as much text as you want before the gif command
    and it will only take words after it as search terms. Before, it
    would take the entire message, regardless of whether it was after
    the command or not.

--- plugins/gif.js
+++ plugins/gif.js
@@ -6,23 +6,29 @@
 var request = require('request');
 
 module.exports.load = function(bot) {
-  bot.onMessage(/!gif .*/, gif);
+  bot.onMessage(/.!gif .*/, gif);
 };
 
 var gif = function(channel, from, message) {
   var self = this;
   var arguments = message.split(' ');
   var query = '';
+  var gotToTheKeywords = false;
   for (var i = 1; i < arguments.length; i++) {
-    query += arguments[i] + ' ';
+    if (arguments[i] == '!gif') {
+      gotToTheKeywords = true;
+    }
+    if(gotToTheKeywords) {
+      query += arguments[i] + ' ';
+    }
   }
   request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&offset=0&q=' + query, function (error, response, body) {
     if (!error) {
       var body = JSON.parse(body);
       var size = body.pagination.count;
-      
+      var log = fs.createWriteStream('log.txt', {'flags': 'a'});
       if (size == 0) {
-        self.message(channel, '@' + from.split(' ').join('') + ' 0 \'' + query + '\' gifs were found :( try another search' );
+        self.message(channel, '@' + from.split(' ').join('') + ' 0 \'' + query + '\' gifs were found :( try another search');
       } else {
         var index = Math.floor(Math.random() * size);
         var gif = body.data[index].images.original.url;
